ce14e4fd13bd5cd565253daf1108f72478e75113
Jason Noh <93170784+JJJAAASSSOONNOOHH@users.noreply.github.com>
2024-01-08 15:48:23 +0900

    2024.01.08 - 노솔진 -> Controller 생성만 완료

--- src/main/java/com/erp/ezen25/controller/MainController.java
+++ src/main/java/com/erp/ezen25/controller/MainController.java
@@ -4,13 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
-public class Ezen25Controller {
-    @GetMapping("/main")
+// 메인 관련 Controller
+public class MainController {
+    @GetMapping("/")
     public String mainView() {
         return "bootstrapHTML/index";
     }
---
+++ src/main/java/com/erp/ezen25/controller/ProductController.java
@@ -0,0 +1,8 @@
+package com.erp.ezen25.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+// 상품 관련 Controller
+public class ProductController {
+}
