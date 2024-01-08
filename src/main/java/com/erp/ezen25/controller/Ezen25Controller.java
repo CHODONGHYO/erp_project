af0bdf1fa4fa685aa2e4f73f2c800e29b6f40337
Cho Donghyo <chodonghyo@naver.com>
2024-01-08 17:15:22 +0900

    1/8 17:15 commit

--- src/main/java/com/erp/ezen25/controller/Ezen25Controller.java
+++ src/main/java/com/erp/ezen25/controller/Ezen25Controller.java
@@ -1,14 +1,28 @@
 package com.erp.ezen25.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Log4j2
+@RequestMapping("/ezen25")
 public class Ezen25Controller {
+    @GetMapping("/login")
+    public void loginGET() {
+        log.info("로그인 페이지로");
+    }
+
     @GetMapping("/main")
-    public String mainView() {
-        return "ezen25/login";
+    public String main() {
+        log.info("본사 메인페이지로");
+        return "/ezen25/headquarters/main";
+    }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "/layout/layout";
     }
 }
